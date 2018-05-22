7739080e3061eda4194ae458202386a67e6be6ea
苦叶子 <lyy@LYM.local>
2018-05-22 12:09:16 +0800

    注释调度测试代码

--- app/__init__.py
+++ app/__init__.py
@@ -55,6 +55,4 @@ def create_app(config_name):
     from .api import api_bp as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
-    scheduler.start()
-
     return app
