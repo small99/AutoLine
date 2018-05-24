b8257d84cac7784208aa0a0a262d7fafce595d7e
苦叶子 <lyy@LYM.local>
2018-05-24 09:30:33 +0800

    修订mysql连接串字符编码

--- config.py
+++ config.py
@@ -66,9 +66,9 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') # + "?charset=utf8" or 'sqlite:///' + os.path.join(basedir, 'AutoLine.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') + "?charset=utf8" # or 'sqlite:///' + os.path.join(basedir, 'AutoLine.sqlite')
     #SQLALCHEMY_ECHO = True
-    TRIGGER_DATABASE_URL = os.environ.get('TRIGGER_DATABASE_URL')
+    TRIGGER_DATABASE_URL = os.environ.get('TRIGGER_DATABASE_URL') + "?charset=utf8"
 
 
     @classmethod
